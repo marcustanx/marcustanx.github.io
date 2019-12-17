0f6aa72581e0b13d2f6f9e7587fe291eda29ee76
Marcus Tan <marcus@mtss.dev>
2019-12-18 01:01:02 +0800

    Refresh runner plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const gulp = require('gulp');
 const sass = require('./build/sass');
-const scripts = require('./build/scripts');
+const scripts = require('./build/scripts').default.default;
 const images = require('./build/images');
 const sync = require('./build/browsersync');
 
-[sass, scripts, images, sync].forEach(task => {
+[sass, scripts, images, sync, linkredirect].forEach(task => {
   task(gulp);
 });
 
